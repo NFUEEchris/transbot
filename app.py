1f42d454aa6b6afeb89f59074412af829fb582e5
NTUTEEChris <74455348+NFUEEchris@users.noreply.github.com>
2025-12-07 21:18:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -169,8 +169,9 @@ def create_rich_menu_1():
         rich_menu_id = line_bot_api.create_rich_menu(
             rich_menu_request=rich_menu_to_create
         ).rich_menu_id
-
-        with open('public/richmenu_1.jpg', 'rb') as image:
+        
+        image_path = os.path.join(os.getcwd(), 'public', 'richmenu_1.jpg')
+        with open(image_path, 'rb') as image:
             line_bot_blob_api.set_rich_menu_image(
                 rich_menu_id=rich_menu_id,
                 body=bytearray(image.read()),
@@ -232,3 +233,4 @@ create_rich_menu_1()
 if __name__ == "__main__":
 
     app.run(port=5000,debug=True)
+
